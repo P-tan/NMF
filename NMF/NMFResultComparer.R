5d12403864a43103002f419fa560930bd5aa0e2d
P-tan <ptanmail@gmail.com>
2015-10-13 23:15:56 +0900

    Add: Plot NMF result

--- NMF/NMFResultComparer.R
+++ NMF/NMFResultComparer.R
@@ -7,3 +7,11 @@ lapply(result_files, function(result_file)
     read.csv(result_file) -> df
     list(file = result_file, data = df[c("Time_msec", "NRV")])
 }) ->  results
+
+plot(results[[1]]$data, type="l", log="y")
+lines(results[[2]]$data, col=2)
+legend("topright",
+       legend = c(lapply(results, function(result) result$file)),
+       lty = 1,
+       col = 1:2
+       )
